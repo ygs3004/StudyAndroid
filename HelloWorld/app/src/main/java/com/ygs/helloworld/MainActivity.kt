28278ef06c710423086ceecf24f1dd2b615cdc21
윤건수 <ygs3004@naver.com>
2023-06-25 05:00:15 +0900

    [202323/06/25] udemy Android 12 및 Kotlin  - 14강: 카운티 앱

--- HelloWorld/app/src/main/java/com/ygs/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/ygs/helloworld/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ygs.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        val myTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0;
+
+        btnClickMe.text = "증가"
+        btnClickMe.setOnClickListener {
+//            btnClickMe.text = "변신성공"
+            timesClicked++
+            myTextView.text = timesClicked.toString();
+
+            Toast.makeText(this, "토스트입니다만?", Toast.LENGTH_LONG).show()
+        }
     }
 }
