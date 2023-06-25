1d63d60cc4b6848263f5ce6a1411efd675a050a5
윤건수 <ygs3004@naver.com>
2023-06-26 00:57:25 +0900

    [2023/06/26] udemy Android 12 및 Kotlin  - 26강: 산술연산자

---
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Operator.kt
@@ -0,0 +1,11 @@
+package com.ygs.kotlinbasics
+
+fun main(){
+
+    // 산술 연산자(+, -, *, /, %)
+    var result = 5+3
+    val a: Double = 5.0
+    val b = 3
+    val resultDouble: Double = (a / b)
+    print(resultDouble)
+}
