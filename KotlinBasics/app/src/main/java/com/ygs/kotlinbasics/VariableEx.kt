b3a59702b8e75217d6825a1023ef983c28e1ca39
윤건수 <ygs3004@naver.com>
2023-06-26 00:18:49 +0900

    [2023/06/26] udemy Android 12 및 Kotlin  - 23강: 변수와 데이터 유형 연습문제

---
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/VariableEx.kt
@@ -0,0 +1,14 @@
+package com.ygs.kotlinbasics
+
+fun main() {
+
+    var varString = "Android Masterclass"
+    var varFloat = 13.37F
+    var varDouble = 3.14159265358979
+    var varByte: Byte = 25
+    var varInt = 2020
+    var varLong: Long = 18881234567
+    var varBoolean = true
+    var varCharacter = 'a'
+
+}
