a65edac3f9bba7f2307eb5078c3904d83afde7ec
윤건수 <ygs3004@naver.com>
2023-06-26 03:20:30 +0900

    [2023/06/26] udemy Android 12 및 Kotlin  - 31강: if문 파트2  - 32강: 식으로 된 if문

--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/If.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/If.kt
@@ -14,15 +14,31 @@ fun main() {
     val age = 31;
     
     if(age >= 30) println("30이 넘었네요")
-    
-    if(age>=21){
+
+
+    val currentAge = if(age>=21){
         println("술 마실수 있습니다.")
+        21
     }else if(age>=18){
         println("투표권이 있어요")
+        18
     }else if(age>=16){
         println("Denis 의 나라에서 운전할 수 있습니다.")
+        16
     }else{
         println("엄청 어리네요!")
+        15
     }
+    println("currentAge $currentAge")
+
+    val name = "GunSoo"
+    if(name=="Denis"){
+        println("어서와 데니스")
+    }else{
+        println("누구신지")
+    }
+
+    val isRainy = true
+    if(isRainy) println("비온다!")
 
 }
---
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/When.kt
@@ -0,0 +1,5 @@
+package com.ygs.kotlinbasics
+
+fun main() {
+    
+}
