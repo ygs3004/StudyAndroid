b6e9baa38350257acc0c68357870f3360a6d6409
윤건수 <ygs3004@naver.com>
2023-06-26 00:13:32 +0900

    [2023/06/26] udemy Android 12 및 Kotlin  - 21강: 데이터 유형 bool, char, string  - 22강: 변수와 데이터 유형 코드

--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Basics.kt
@@ -5,19 +5,39 @@ fun main(){
     // val => immutable
     val isVal = "변경불가"
 
+    // var => mutable
     // type string
     var myName = "Yoon un Soo"
-    // type int 32bit => 타입추론
+    // type int 32bit
+    // 타입이 적혀있지 않을경우 => 타입추론
     var myAge = 33
     
     // Integer TYPES: Byte(8bit), Short(16bit), Int(32bit), Long(64bit)
     val myByte: Byte = 127
     val myShort: Short = 259
-    val myInt: Int = 13213123
-    val myLong: Long = 12_03_9123123123213
+    val myInt: Int = 13_213_123
+    val myLong: Long = 12_039_123_123_123_213
+    println(myInt)
+    println(myLong)
 
     // Floating TYPES: Float(32bit), Double(64bit)
     val myFloat: Float = 1.1F
     val myDouble: Double = 3.14162592
+
+    // Boolean: true and false
+    // var isSunny: Boolean = true
+    var isSunny = true
+    isSunny = false
+
+    // Characters
+    val letterChar = 'A'
+    val digitChar = '1'
+
+    // String
+    val myStr = "Hello World"
+    val firstCharInStr = myStr[0]
+    val lastCharInStr = myStr[myStr.length-1]
+    println("First Character: $firstCharInStr")
+    print("Last Character: $lastCharInStr")
 
 }
