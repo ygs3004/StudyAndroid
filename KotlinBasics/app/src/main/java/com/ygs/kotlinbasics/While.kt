2c07157bcea3e43fb2e467c37854e22ff58586c1
윤건수 <ygs3004@naver.com>
2023-06-27 02:08:24 +0900

    [2023/06/26] udemy Android 12 및 Kotlin  - 36강: while 문

---
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/While.kt
@@ -0,0 +1,17 @@
+package com.ygs.kotlinbasics
+
+fun main() {
+    var x = 1
+    while (x <= 10) {
+        println("$x")
+        x++
+    }
+    println("while 종료")
+
+    var y = 100
+    while (y >= 0) {
+        println("$y")
+        y -= 2
+    }
+    println("while 종료2")
+}
