5bc554ab171d7b909a12115c570e3714c0da17e5
윤건수 <ygs3004@naver.com>
2023-06-27 23:28:25 +0900

    [2023/06/27] udemy Android 12 및 Kotlin  - 47강: Nullables(엘비스 연산자), Not Null 단정

--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Nullable.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Nullable.kt
@@ -3,23 +3,30 @@ package com.ygs.kotlinbasics
 import java.util.*
 
 fun main() {
-    var name :String = "Ygs"
+    // var name :String = "Ygs"
     // name = null -> 컴파일에러
     var nullableName : String? = "Ygs"
-    // nullableName = null
+      nullableName = null
 
-    var len = name.length
+    // var len = name.length
 
     var len2 = nullableName?.length
     // println(nullableName?.toLowerCase())
-    nullableName?.let { println(it.length) }
+    nullableName?.let { println(it.toString().length) }
 
+    // ?: Elvis Operator
+    val name = nullableName ?: "Guest"
+    //println("name is $name")
 
+    println(nullableName!!.toString().toLowerCase());
 
     // if(nullableName != null){
     //     var len2 = nullableName.length
     // }else{
     //     null
     // }
+
+    // chain safe calls
+    // val wifeAge: String? = user?.wife?.age ?: 0
 
 }
