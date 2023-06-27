7a9c6b96b2488e2bd1eb29f4fc111bcba5a94e35
윤건수 <ygs3004@naver.com>
2023-06-28 02:18:41 +0900

    [2023/06/28] udemy Android 12 및 Kotlin  - 57강: 클래스 Member Variable and Function

--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Class.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Class.kt
@@ -1,16 +1,39 @@
 package com.ygs.kotlinbasics
 
 fun main() {
-    var ygs = Person("GS", "Yoon")
+    var ygs = Person("GS", "Yoon", 31)
     var ygs2:Person = Person()
-    var blahYoon = Person("blah")
-    var GunsooKim = Person(lastName = "Kim")
+    // var blahYoon = Person("blah")
+    // var GunsooKim = Person(lastName = "Kim")
+    ygs.hobby = "독서"
+    ygs.age = 21;
+    println("Ygs is ${ygs.age} years old")
+    ygs.stateHobby()
+
+    ygs2.hobby = "숙면"
+    ygs2.stateHobby()
 }
 
 class Person (firstName: String = "Gunsoo", lastName: String = "Yoon"){
+    //Member Variables - Properties
+    var age: Int? = null
+    var hobby: String = "게임"
+    var firstName: String? = null
 
     // Initializer Block
     init {
-        println("사람을 만든다! 이름은 $firstName, 성은 $lastName 입니다~")
+        this.firstName = firstName
+        println("안녕? 내 이름은 ${firstName}라고해, 성은 ${lastName}고")
+    }
+
+    // Member secondary Constructor
+    constructor(firstName: String, lastName: String, age: Int) : this(firstName, lastName) {
+        this.age = age
+        println("안녕? 내 이름은 ${firstName}라고해, 성은 ${lastName}고 ${age}살이야")
+    }
+
+    // Member functions - Methods
+    fun stateHobby(){
+        println("${firstName} 취미는 ${hobby}야 ㅎㅎ")
     }
 }
