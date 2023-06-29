db12983f69ae2ed77a5c2d873255c69c2e28ee1f
윤건수 <ygs3004@naver.com>
2023-06-29 23:35:24 +0900

    [2023/06/29] udemy Android 12 및 Kotlin  - 64강: 인터페이스

--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Heritance.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/Heritance.kt
@@ -10,8 +10,16 @@ open class Vehicle{
 // sealed class Car => 상속 불가
 // open class Car2: Vehicle(){ => 상속 가능
 
+interface Drivable{
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("멈춰")
+    }
+}
 
-open class Car2(val name: String, val brand: String){
+
+open class Car(override val maxSpeed: Double, val name: String, val brand: String): Drivable{
     open var range: Double = 0.0
 
     fun extendRagne(amount: Double) {
@@ -20,6 +28,11 @@ open class Car2(val name: String, val brand: String){
         }
     }
 
+//    override fun drive(): String{
+//        return "driving the interface drive"
+//    }
+    override fun drive(): String = "인터페이스 운전 중"
+
     open fun drive(distance: Double) {
         println("이 차의 주행거리는 $distance 입니다.")
     }
@@ -27,7 +40,7 @@ open class Car2(val name: String, val brand: String){
 
 }
 
-class ElectricCar(name: String, brand: String, batteryLif: Double): Car2(name, brand){
+class ElectricCar(override val maxSpeed:Double, name: String, brand: String, batteryLif: Double): Car(maxSpeed, name, brand){
     override var range = batteryLif * 6
 
     var chargerType = "Type1"
@@ -36,24 +49,31 @@ class ElectricCar(name: String, brand: String, batteryLif: Double): Car2(name, b
         println("이 전기차의 주행거리는 $range 입니다.")
     }
 
-    fun drive() {
-        println("이 전기차의 주행거리는 $range 입니다.")
+    override fun drive():String {
+        return "이 전기차의 주행거리는 $range 입니다."
+    }
+
+    override fun brake() {
+        super.brake()
+        println("전기차가 멈췄습니다.")
     }
 }
 
 
 // Any class inherits from the Any Class -> 자바의 Object
 fun main() {
-    var audiA3 = Car2("A3", "Audi")
-    var teslaS = ElectricCar("S-Model", "Tesla", batteryLif = 85.0)
+    var audiA3 = Car(200.0, "A3", "Audi")
+    var teslaS = ElectricCar(240.0, "S-Model", "Tesla", batteryLif = 85.0)
 
     teslaS.chargerType = "Type2"
 
     teslaS.extendRagne(200.0)
-    teslaS.drive()
+//    teslaS.drive()
+    teslaS.brake();
+    audiA3.brake()
 
     // 다형성
-    audiA3.drive(200.0)
-    teslaS.drive(200.0)
+//    audiA3.drive(200.0)
+//    teslaS.drive(200.0)
 
 }
--- KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/LateInitGetSet.kt
+++ KotlinBasics/app/src/main/java/com/ygs/kotlinbasics/LateInitGetSet.kt
@@ -1,38 +1,38 @@
 package com.ygs.kotlinbasics
 
 import java.lang.IllegalArgumentException
-
-fun main() {
-    var myCar = Car();
-    println("내 차 브랜드: ${myCar.myBrand}")
-    // myCar.maxSpeed = 2
-    println("내 차 최고속도: ${myCar.maxSpeed}")
-    println("내 차 모델: ${myCar.myModel}")
-
-}
-
-class Car(){
-
-    lateinit var owner: String
-    val myBrand: String = "BMW"
-        // Custom getter
-        get(){
-            return field.toLowerCase();
-        }
-
-    var maxSpeed: Int = 250
-        // normal getter, setter
-        // get() = field
-        set(value){
-            field = if(value>0) value else throw IllegalArgumentException("0 보다 작은속도는 안돼!");
-        }
-
-    var myModel:String = "M5"
-    private set
-
-    init{
-        this.myModel = "M3" // private set 안에서는 가능
-        this.owner = "Frank"
-    }
-
-}
+//
+//fun main() {
+//    var myCar = Car();
+//    println("내 차 브랜드: ${myCar.myBrand}")
+//    // myCar.maxSpeed = 2
+//    println("내 차 최고속도: ${myCar.maxSpeed}")
+//    println("내 차 모델: ${myCar.myModel}")
+//
+//}
+//
+//class Car(){
+//
+//    lateinit var owner: String
+//    val myBrand: String = "BMW"
+//        // Custom getter
+//        get(){
+//            return field.toLowerCase();
+//        }
+//
+//    var maxSpeed: Int = 250
+//        // normal getter, setter
+//        // get() = field
+//        set(value){
+//            field = if(value>0) value else throw IllegalArgumentException("0 보다 작은속도는 안돼!");
+//        }
+//
+//    var myModel:String = "M5"
+//    private set
+//
+//    init{
+//        this.myModel = "M3" // private set 안에서는 가능
+//        this.owner = "Frank"
+//    }
+//
+//}
