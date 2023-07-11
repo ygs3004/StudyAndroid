1f894ee64a7d6473f2ec4333ccf82b7c4968909b
윤건수 <ygs3004@naver.com>
2023-07-11 22:33:27 +0900

    [2023/07/11] udemy Android 12 및 Kotlin - 섹션 6: 분 나이 계산기  > 87강: DatePicker Dialog, onDataSetListener 추가

--- DOBCalc/app/src/main/java/com/ygs/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/com/ygs/dobcalc/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.ygs.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener { view ->
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            { view, year, month, dayOfMonth ->
+                Toast.makeText(this,
+                    "날짜를 선택하세요", Toast.LENGTH_LONG).show()
+            },
+            year,
+            month,
+            day
+        ).show()
+
+
     }
 }
