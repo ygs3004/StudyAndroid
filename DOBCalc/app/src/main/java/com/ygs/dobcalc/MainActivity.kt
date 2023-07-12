ac85618edb36c7c87859dfcf393e477cfa3a6b98
윤건수 <ygs3004@naver.com>
2023-07-12 22:22:21 +0900

    [2023/07/12] udemy Android 12 및 Kotlin - 섹션 6: 분 나이 계산기  > 88강: simpleDateFormat 과 날짜 확인하기

--- DOBCalc/app/src/main/java/com/ygs/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/com/ygs/dobcalc/MainActivity.kt
@@ -4,15 +4,21 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
 
         btnDatePicker.setOnClickListener { view ->
             clickDatePicker()
@@ -21,15 +27,25 @@ class MainActivity : AppCompatActivity() {
 
     fun clickDatePicker(){
 
+        // 현재 날짜 정보
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
         DatePickerDialog(this,
-            { view, year, month, dayOfMonth ->
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 Toast.makeText(this,
-                    "날짜를 선택하세요", Toast.LENGTH_LONG).show()
+                    "선택된 년도는 $selectedYear, 달은 ${selectedMonth+1} 일자는 ${selectedDayOfMonth} 입니다.", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedYear/${selectedMonth+1}/$selectedDayOfMonth"
+
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA)
+
+                val theDate = sdf.parse(selectedDate)
+
             },
             year,
             month,
