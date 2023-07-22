5b43ff15b0f962faaa77dd6522a1d14a85bba849
윤건수 <ygs3004@naver.com>
2023-07-22 22:19:57 +0900

    [2023/07/22] udemy Android 12 및 Kotlin - 섹션 6: 계산기 > 95강: onClear 기능 추가

--- Calculator/app/src/main/java/com/ygs/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/ygs/calculator/MainActivity.kt
@@ -3,15 +3,27 @@ package com.ygs.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var textViewInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewInput = findViewById(R.id.textViewInput)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        // Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        textViewInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        textViewInput?.text = ""
     }
 }
