c54ebe3acbc1bb3954150aa12794bebbf3a33cf5
윤건수 <ygs3004@naver.com>
2023-08-02 00:52:41 +0900

    [2023/08/92] udemy Android 12 및 Kotlin - 섹션 8: QUIZ APP  > 104강: 질문 모델 생성하고 질문 준비하기

--- MyQuizApp/app/src/main/java/com/ygs/myquizapp/MainActivity.kt
+++ MyQuizApp/app/src/main/java/com/ygs/myquizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.ygs.myquizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val etName: EditText = findViewById(R.id.et_name)
+        btnStart.setOnClickListener {
+
+            if(etName.text.isEmpty()){
+                Toast.makeText(this, "이름을 입력해주세요", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ MyQuizApp/app/src/main/java/com/ygs/myquizapp/QuizQuestionActivity.kt
@@ -0,0 +1,16 @@
+package com.ygs.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class QuizQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_question)
+
+        val questionsList = Constants.getQuestions()
+        Log.i("QuestionsList 크기: ", "${questionsList.size}")
+
+    }
+}
