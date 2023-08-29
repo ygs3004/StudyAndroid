446485165ec314c145318a9f151cf855d6c5d833
윤건수 <ygs3004@naver.com>
2023-08-29 23:11:14 +0900

    [2023/08/29] udemy Android 12 및 Kotlin - 섹션 9: 그리기 APP > 122강: Custom Drawable 팔레트 추가

--- Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
+++ Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
@@ -4,16 +4,27 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+        val linearLayoutPaintColor = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColor[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener(){
