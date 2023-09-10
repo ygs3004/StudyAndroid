71e38f80e8cff2bb39513966259c9bec6a4b6397
윤건수 <ygs3004@naver.com>
2023-09-10 21:16:29 +0900

    [2023/09/10] udemy Android 12 및 Kotlin - 섹션 9: 그리기 APP > 123강: 색상 선택 기능 추가

--- Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
+++ Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
@@ -3,8 +3,10 @@ package com.ygs.drawing
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -56,5 +58,26 @@ class MainActivity : AppCompatActivity() {
 
         brushDialog.show()
     }
-    
+
+    fun paintClicked(view: View) {
+        // Toast.makeText(this, "clicked paint", Toast.LENGTH_LONG).show()
+        if(view !== mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString();
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
+
+    }
+
+
 }
