42a9e73a1c8fc745714882cf5b8a267376d37999
윤건수 <ygs3004@naver.com>
2023-09-24 00:55:46 +0900

    [2023/09/24] udemy Android 12 및 Kotlin - 섹션 9: 그리기 APP > 130강: 권한 요청 추가하기

--- Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
+++ Drawing/app/src/main/java/com/ygs/drawing/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.ygs.drawing
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +9,9 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -14,6 +19,28 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(
+                        this,
+                        "내부 저장소에 접근을 허락했습니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }else{
+                    if(permissionName == Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this,
+                            "접근 권한을 거부하셨습니다.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +58,11 @@ class MainActivity : AppCompatActivity() {
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener(){
             showBrushSizeChooserDialog()
+        }
+
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener{
+            requestStoragePermission()
         }
     }
 
@@ -76,8 +108,31 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
-
     }
 
+    private fun requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog("그리기!", "내부저장소 권한이 필요합니다.")
+        }else {
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+                // TODO - external storage permission 요청
+            ))
+        }
+    }
+
+    private fun showRationaleDialog(title: String, message: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("취소"){ dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
 
 }
