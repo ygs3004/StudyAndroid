8d422f3537e1164447eaf0203e82f3587aace8ad
윤건수 <ygs3004@naver.com>
2023-10-09 17:10:31 +0900

    [2023/10/09] udemy Android 12 및 Kotlin - 섹션 10: 7분 운동앱 > 140강: 뷰바인딩 사용하기

--- Workout/app/src/main/java/com/ygs/workout/MainActivity.kt
+++ Workout/app/src/main/java/com/ygs/workout/MainActivity.kt
@@ -2,16 +2,21 @@ package com.ygs.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.Toast
+import com.ygs.workout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
+        //val flStartButton: FrameLayout = findViewById(R.id.flStart)
+
+        binding?.flStart?.setOnClickListener{
             Toast.makeText(
                 this@MainActivity,
                 "시작!",
@@ -19,4 +24,10 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
 }
