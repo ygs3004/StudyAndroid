1edb3300cdda9ec177d4d9d578c87ba899132bde
윤건수 <ygs3004@naver.com>
2023-10-15 23:13:23 +0900

    [2023/10/15] udemy Android 12 및 Kotlin - 섹션 10: 7분 운동앱 > 145강: 타이머 추가 및 전환

--- Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
+++ Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
@@ -3,6 +3,7 @@ package com.ygs.workout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.ygs.workout.databinding.ActivityExerciseBinding
 
@@ -12,6 +13,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +59,41 @@ class ExerciseActivity : AppCompatActivity() {
                     "운동을 시작하세요!",
                     Toast.LENGTH_LONG
                 ).show()
+
+                setupExerciseView()
+            }
+        }.start()
+
+    }
+
+    private fun setupExerciseView(){
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "운동중입니다!"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar(){
+        binding?.progressBarExercise?.progress = exerciseProgress
+        exerciseTimer = object: CountDownTimer(30000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "운동시간 종료! 휴식시간입니다",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }.start()
 
@@ -66,6 +105,11 @@ class ExerciseActivity : AppCompatActivity() {
         if (restTimer != null) {
             restTimer?.cancel()
             restProgress = 0
+        }
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
         }
 
         binding = null
