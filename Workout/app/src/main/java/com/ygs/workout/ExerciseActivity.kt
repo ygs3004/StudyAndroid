639c3fe2e7716bb817749073454b0208b0451bfd
윤건수 <ygs3004@naver.com>
2023-10-24 00:10:06 +0900

    [2023/10/24] udemy Android 12 및 Kotlin - 섹션 10: 7분 운동앱 > 152강: 미디어 플레이어 소리 추가

--- Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
+++ Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
@@ -1,5 +1,7 @@
 package com.ygs.workout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -8,6 +10,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.ygs.workout.databinding.ActivityExerciseBinding
+import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -25,6 +28,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var currentExercisePosition = -1
 
     private var tts: TextToSpeech? = null
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +53,16 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     private fun setupRestView(){
+
+        try{
+            val soundURI = Uri.parse("android.resource://com.ygs.workout/" + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
         binding?.flRestView?.visibility = View.VISIBLE
         binding?.tvTitle?.visibility = View.VISIBLE
         binding?.tvExerciseName?.visibility = View.INVISIBLE
@@ -153,6 +167,10 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         if (tts != null) {
             tts!!.stop()
             tts!!.shutdown()
+        }
+
+        if (player != null) {
+            player!!.stop()
         }
 
         binding = null
