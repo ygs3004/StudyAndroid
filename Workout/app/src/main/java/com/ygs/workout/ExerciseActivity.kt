02038ab8df6ba7312767942fd8eede755e42e83c
윤건수 <ygs3004@naver.com>
2023-10-28 18:06:52 +0900

    [2023/10/28] udemy Android 12 및 Kotlin - 섹션 10: 7분 운동앱 > 157강: Recyclerview 화면 적용

--- Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
+++ Workout/app/src/main/java/com/ygs/workout/ExerciseActivity.kt
@@ -9,6 +9,7 @@ import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ygs.workout.databinding.ActivityExerciseBinding
 import java.lang.Exception
 import java.util.*
@@ -30,6 +31,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var tts: TextToSpeech? = null
     private var player: MediaPlayer? = null
 
+    private var exerciseAdapter: ExerciseStatusAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -49,7 +52,15 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
 
         setupRestView()
+        setupExerciseStatusRecyclerView()
+    }
 
+    private fun setupExerciseStatusRecyclerView() {
+        binding?.rvExerciseStatus?.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        exerciseAdapter = ExerciseStatusAdapter(exerciseList!!)
+        binding?.rvExerciseStatus?.adapter = exerciseAdapter
     }
 
     private fun setupRestView(){
