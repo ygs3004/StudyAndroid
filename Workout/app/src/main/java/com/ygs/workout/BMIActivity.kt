8b8c2374044d7b6e68792de447047b60285c0bbb
윤건수 <ygs3004@naver.com>
2023-11-07 23:27:09 +0900

    [2023/11/07] udemy Android 12 및 Kotlin - 섹션 10: 7분 운동앱 > 162강: BMI 계산기 추가

--- Workout/app/src/main/java/com/ygs/workout/BMIActivity.kt
+++ Workout/app/src/main/java/com/ygs/workout/BMIActivity.kt
@@ -2,7 +2,11 @@ package com.ygs.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.ygs.workout.databinding.ActivityBmiBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
 
@@ -23,6 +27,69 @@ class BMIActivity : AppCompatActivity() {
         binding?.toolbarBmiActivity?.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        binding?.btnCalculateUnits?.setOnClickListener {
+            if(validateMetricUnits()){
+                val heightValue: Float = binding?.etMetricUnitHeight?.text.toString().toFloat() / 100
+                val weightValue: Float = binding?.etMetricUnitWeight?.text.toString().toFloat()
+                val bmi = weightValue/ (heightValue * heightValue)
+
+                displayBMIResult(bmi)
+            }else{
+                Toast.makeText(this@BMIActivity, "입력된 값이 유효하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun displayBMIResult(bmi: Float) {
+
+        var bmiLabel: String
+        var bmiDescription: String
+
+        if(bmi.compareTo(15f) <= 0){
+            bmiLabel = "아주 심각한 저체중"
+            bmiDescription = "이런! 자신을 더 돌볼 필요가 있어요! 더 많이 먹어주세요"
+        }else if(bmi.compareTo(15f) > 0 && bmi.compareTo(16f) <=0){
+            bmiLabel = "심각한 저체중"
+            bmiDescription = "이런! 자신을 더 돌볼 필요가 있어요! 더 많이 먹어주세요"
+        }else if(bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <=0){
+            bmiLabel = "저체중"
+            bmiDescription = "이런! 자신을 더 돌볼 필요가 있어요! 더 많이 먹어주세요"
+        }else if(bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <=0){
+            bmiLabel = "보통"
+            bmiDescription = "아주 좋습니다! 지금의 상태를 잘 유지해주세요"
+        }else if(bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <=0){
+            bmiLabel = "과체중"
+            bmiDescription = "이런! 자신을 더 돌볼 필요가 있어요! 운동에 신경쓰세요"
+        }else if(bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <=0){
+            bmiLabel = "비만"
+            bmiDescription = "이런! 자신을 더 돌볼 필요가 있어요! 운동에 신경쓰세요"
+        }else if(bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <=0){
+            bmiLabel = "심각한 비만"
+            bmiDescription = "이럴수가! 굉장히 위험한 상태입니다! 운동을 꼭 하셔야 합니다!"
+        }else{
+            bmiLabel = "아주 심각한 비만"
+            bmiDescription = "이럴수가! 굉장히 위험한 상태입니다! 운동을 꼭 하셔야 합니다!"
+        }
+
+        val bmiValue = BigDecimal(bmi.toDouble()).setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        binding?.llDisplayBMIResult?.visibility = View.VISIBLE
+        binding?.tvBMIValue?.text = bmiValue
+        binding?.tvBMIType?.text = bmiLabel
+        binding?.tvBMIDescription?.text = bmiDescription
+    }
+
+    private fun validateMetricUnits(): Boolean{
+        var isValid = true
+
+        if(binding?.etMetricUnitWeight?.text.toString().isEmpty()){
+            isValid = false
+        }else if(binding?.etMetricUnitHeight?.text.toString().isEmpty()) {
+            isValid = false
+        }
+
+        return isValid
     }
 
 }
