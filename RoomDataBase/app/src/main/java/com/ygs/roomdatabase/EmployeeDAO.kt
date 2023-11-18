9d870605c5b65d3652594782cd807c6f76cf42b1
윤건수 <ygs3004@naver.com>
2023-11-19 00:58:18 +0900

    [2023/11/19] udemy Android 12 및 Kotlin - 섹션 10: Room Database Demo > 170강: @Database 생성

---
+++ RoomDataBase/app/src/main/java/com/ygs/roomdatabase/EmployeeDAO.kt
@@ -0,0 +1,24 @@
+package com.ygs.roomdatabase
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDAO {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("SELECT * FROM `employee-table`")
+    fun fetchAllEmployees(): Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM `employee-table` where id=:id")
+    fun fetchAllEmployeeById(id:Int): Flow<EmployeeEntity>
+
+}
