334fce1f8f3d31aca9aa750b2104d2490cfb9e81
윤건수 <ygs3004@naver.com>
2023-12-18 19:59:57 +0900

    [2023/12/18] udemy Android 12 및 Kotlin - 섹션 11: Happy Places(장소, GPS, 구글 맵 API)  > 183강: DatePicker 추가

--- HappyPlaces/app/src/main/java/com/ygs/happyplaces/AddHappyPlaceActivity.kt
+++ HappyPlaces/app/src/main/java/com/ygs/happyplaces/AddHappyPlaceActivity.kt
@@ -1,10 +1,19 @@
 package com.ygs.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -13,6 +22,35 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         toolbar_add_place.setNavigationOnClickListener{
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        et_date.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
     }
 
 }
